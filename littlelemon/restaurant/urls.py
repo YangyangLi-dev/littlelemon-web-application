c509f8d42780a83e635cc2fa0069b1e184195178
liyy <yangyang.li6@amh-group.com>
2023-05-17 19:58:40 +0800

    add urls

---
+++ littlelemon/restaurant/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+router = DefaultRouter(trailing_slash = False)
+router.register("users", views.UserViewSet, basename = "user")
+router.register("menu-items", views.MenuViewSet, basename = "menu-items")
+router.register("bookings", views.BookingViewSet, basename = "bookings")
+
+urlpatterns = [
+    path("",include(router.urls)),
+    path("home/", views.home, name="home"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api-token-auth/", obtain_auth_token),  
+    path('about/', views.about, name="about"),
+    path('menu/',views.menu,name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+]
